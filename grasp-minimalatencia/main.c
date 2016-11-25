90f2ae85382fbe539a08dd1edb34958f9a2f0101
gleisson.assis@gmail.com <gleissonassis@Gleissons-MBP.lan>
2016-11-25 21:51:44 -0200

    Cópia inicial dos arquivios

---
+++ grasp-minimalatencia/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  grasp-minimalatencia
+//
+//  Created by Gleisson Assis on 11/25/16.
+//  Copyright © 2016 Gleisson de Assis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
